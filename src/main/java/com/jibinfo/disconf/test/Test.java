16a84dc58328c044c5f1cb514e61a0f343f9266b
The_Ge <549123178@qq.com>
2017-02-16 14:21:19 +0800

    临时添加方法

---
+++ src/main/java/com/jibinfo/disconf/test/Test.java
@@ -0,0 +1,23 @@
+package com.jibinfo.disconf.test;
+
+import java.math.BigInteger;
+
+/**
+ * Created by admin on 2017/2/16.
+ */
+public class Test {
+
+
+    public static void main(String[] args) {
+        String string = " DELETE from interface_log_response where id BETWEEN %s AND %s;";
+        int startIndex = 7100000;
+        int step = 50000;
+        for (int i = 1; i < 60; i++) {
+            int endIndex = startIndex + step;
+            System.out.println(String.format(string, startIndex,endIndex));
+
+            startIndex = endIndex;
+        }
+
+    }
+}
